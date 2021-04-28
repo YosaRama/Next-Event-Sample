087405286e00d705d2990f03a3078468d47401f9
yosaramadinata <yosaramadinata@gmail.com>
2021-04-28 21:54:40 +0800

    Remove all testing routing file

--- pages/blog/index.js
+++
@@ -1,7 +0,0 @@
-function BlogListPage() {
-  return (
-    <div>
-      <h1>This is BlogListPage</h1>
-    </div>
-  );
-}
--- pages/portfolio/index.js
+++
@@ -1,9 +0,0 @@
-function PortfolioPage() {
-  return (
-    <div>
-      <h1>This is PortfolioPage</h1>
-    </div>
-  );
-}
-
-export default PortfolioPage;
